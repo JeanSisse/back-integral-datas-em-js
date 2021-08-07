bf6a4565b356ceeaaa3959d0e9f921e236a2f9d2
JeanSisse <tavares1988@live.com>
2021-08-07 06:39:57 -0300

    Exercicio class01 realizado

---
+++ classe-01/index.js
@@ -0,0 +1,11 @@
+// Criando Datas
+
+//Cria um momento (o momento atual)
+function getMomentoAtual(){
+    return momentoAtual = new Date();
+}
+
+const dataEspecifico = new Date(2020, 10, 24, 21, 30);
+console.log(dataEspecifico);
+
+console.log(getMomentoAtual());
